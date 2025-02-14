380757c2a5e37d731e12d61764f3aad2365c70f7
namansoni810 <namanxsoni@gmail.com>
2025-02-14 08:16:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,15 +54,15 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "pakka?",
+    "sahi me pakka?",
+    "itni sure he tu?",
+    "please yaar yes karde",
+    "soch to sahiiiiii",
+    "no mat bol yaar :< ",
+    "me ro dunga",
+    "me bohot jyada ro dunga",
+    "theek he ab nahi puchra",
     "Just kidding, say yes please! ❤️"
 ];
 
